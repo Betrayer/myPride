04f2e9b4e69c17fa864c04bba20018b2d689a69a
Betrayer <StarDestroyer@ukr.net>
2021-03-09 18:49:51 +0200

    list succesfully formed

--- src/components/form/inputs/Intputs.tsx
+++ src/components/form/inputs/Intputs.tsx
@@ -5,21 +5,37 @@ const Intputs = () => {
   const [name, setName] = useState("");
   const [number, setNumber] = useState("");
 
-  //   const [user, setUser] = useState({
-  //     name: "",
-  //     number: "",
-  //   });
+  const contactsList = JSON.parse(localStorage.getItem("contacts") || "[]");
 
-  const wtf = (e: React.FormEvent) => {
+  interface User {
+    name: string;
+    number: number;
+  }
+
+  const handleForm = (e: React.FormEvent) => {
     e.preventDefault();
     const formData = new FormData(e.target as HTMLFormElement);
-    const a = formData.get("user_name");
-    console.log("a :>> ", a);
+    const getName = formData.get("user_name");
+    const getNumber = Number(formData.get("user_number"));
+    const user: User = {
+      name: `${getName}`,
+      number: getNumber,
+    };
+    if (!Number.isNaN(getNumber)) {
+      // const contactsList = JSON.parse(localStorage.getItem("contacts") || "[]");
+      contactsList.push(user);
+      localStorage.setItem("contacts", JSON.stringify(contactsList));
+    }
   };
 
   return (
-    <section className={css.formSection}>w
-      <form action="/my_contacts" method="post" className={css.addingForm} onSubmit={wtf}>
+    <section className={css.formSection}>
+      <form
+        action="/my_contacts"
+        method="post"
+        className={css.addingForm}
+        onSubmit={handleForm}
+      >
         <p className={css.contactsHeader}>Contacts</p>
         <ul className={css.formItemsList}>
           <li>
@@ -52,65 +68,5 @@ const Intputs = () => {
     </section>
   );
 };
-
-// import React, { Component } from "react";
-// import css from "./inputs.module.css";
-
-// class Intputs extends Component {
-//   state = {
-//     name: "",
-//     number: "",
-//   };
-
-//   handleChange = (e: any) => {
-//     this.setState({
-//       [e.target.name]: e.target.value,
-//     });
-//   };
-
-//   handleSubmit = (e: any) => {
-//       e.preventDefault();
-//       if(!Number.isNaN(this.state.number){
-
-//       }
-//   }
-
-//   render() {
-//     return (
-//       <section className={css.formSection}>
-//         <form action="URL" method="post" className={css.addingForm}>
-//           <p className={css.contactsHeader}>Contacts</p>
-//           <ul className={css.formItemsList}>
-//             <li>
-//               <label htmlFor="name">Name:</label>
-//               <input
-//                 id="user_name"
-//                 name="name"
-//                 type="text"
-//                 placeholder="Ex.: John Doe"
-//                 onChange={this.handleChange}
-//                 value={this.state.name}
-//               ></input>
-//             </li>
-//             <li>
-//               <label htmlFor="number">Phone number:</label>
-//               <input
-//                 id="user_number"
-//                 name="number"
-//                 type="tel"
-//                 placeholder="Ex.: 8800553535"
-//                 onChange={this.handleChange}
-//                 value={this.state.number}
-//               ></input>
-//             </li>
-//             <li>
-//               <button type="submit">Add contact</button>
-//             </li>
-//           </ul>
-//         </form>
-//       </section>
-//     );
-//   }
-// }
 
 export default Intputs;
